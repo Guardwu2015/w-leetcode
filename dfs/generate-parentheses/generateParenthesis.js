a0309c9728d0aae6f7a00448138d563ee9bda173
wuyue <wujingwei@mafengwo.cn>
2020-11-26 13:50:47 +0800

    feat: 添加LC22, 括号生成

---
+++ dfs/generate-parentheses/generateParenthesis.js
@@ -0,0 +1,37 @@
+// https://leetcode-cn.com/problems/generate-parentheses/submissions/
+
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+const generateParenthesis = function (n) {
+    if (n === 0) {
+        return [""]
+    }
+    const res = []
+    backtrack(n, n, [], res)
+    return res
+};
+
+function backtrack(left, right, track, res) {
+    if (right < left) {
+        return
+    }
+    if (left < 0 || right < 0) {
+        return
+    }
+    if (left == 0 && right == 0) {
+        res.push(track.join(''))
+        return
+    }
+
+    // 尝试放一个左括号
+    track.push('(') // 选择
+    backtrack(left - 1, right, track, res)
+    track.pop() // 撤消选择
+
+    // 尝试放一个右括号
+    track.push(')') // 选择
+    backtrack(left, right - 1, track, res)
+    track.pop() // 撤消选择
+}
