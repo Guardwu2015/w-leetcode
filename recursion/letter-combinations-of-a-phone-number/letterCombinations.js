4b3c81a2f36085cdc06888ae8d354f0e4aa0b43c
武岳 <guardwu@126.com>
2020-11-28 20:48:30 +0800

    feat: 添加LC17. 电话号码的字母组合

---
+++ recursion/letter-combinations-of-a-phone-number/letterCombinations.js
@@ -0,0 +1,30 @@
+// https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number/submissions/
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+
+const letterMap = ['', '', 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz']
+
+const letterCombinations = function(digits) {
+    const res = []
+    if (digits === '') {
+        return res
+    }
+
+    findCombinations(digits, 0, '', res)
+    return res
+};
+
+function findCombinations(digits, index, s, res) {
+    if (digits.length === s.length) {
+        res.push(s)
+    }
+    const c = Number(digits.charAt(index))
+    const letter = letterMap[c]
+    for (let i = 0; i < letter.length; i++) {
+        findCombinations(digits, index + 1, s + letter[i], res)
+    }
+
+    return
+}
