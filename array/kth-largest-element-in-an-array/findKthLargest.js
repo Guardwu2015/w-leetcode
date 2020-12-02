963e16fbf1eaece1597bf2cf7841814f0650ad10
武岳 <guardwu@126.com>
2020-12-02 22:10:14 +0800

    feat: LC215. 数组中的第K个最大元素

---
+++ array/kth-largest-element-in-an-array/findKthLargest.js
@@ -0,0 +1,53 @@
+// https://leetcode-cn.com/problems/kth-largest-element-in-an-array
+
+/**
+ * 思路一，快排
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function (nums, k) {
+  let lo = 0, hi = nums.length - 1;
+
+    k = nums.length - k;
+    while (lo <= hi) {
+        let p = partition(nums, lo, hi);
+        if (p < k) {
+            lo = p + 1;
+        } else if (p > k) {
+            hi = p - 1;
+        } else {
+            return nums[p];
+        }
+    }
+    return -1;
+}
+
+function partition(nums, lo, hi) {
+    if (lo == hi) return lo;
+    const pivot = nums[lo];
+    let i = lo, j = hi + 1;
+    while (true) {
+        while (nums[++i] < pivot) {
+            if (i == hi) break;
+        }
+        while (nums[--j] > pivot) {
+            if (j == lo) break;
+        }
+        if (i >= j) break;
+
+        swap(nums, i, j);
+    }
+
+    swap(nums, j, lo);
+
+    return j;
+}
+
+function swap(nums, i, j) {
+    [nums[i], nums[j]] = [nums[j], nums[i]]
+}
+
+/**
+ * 思路二，二叉堆
+ */
