d534f9b074d143dd19b714794c93c94051547ee1
wuyue <wujingwei@mafengwo.cn>
2020-12-21 09:45:16 +0800

    feat: LC300. 最长递增子序列

---
+++ dp/longest-increasing-subsequence/lengthOfLIS-1.js
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/longest-increasing-subsequence/description/
+
+/**
+ * 思路：记忆化搜索
+ * 时间复杂度: O(n ^ 2)
+ * 空间复杂度: O(n)
+ * @param {number[]} nums
+ * @return {number}
+ */
+let memo = []
+const lengthOfLIS = function (nums) {
+    if (nums.length === 0) {
+        return 0
+    }
+
+    memo = new Array(nums.length).fill(-1)
+    let res = 1
+    for (let i = 0; i < nums.length; i++) {
+        res = Math.max(res, getMaxLength(nums, i))
+    }
+    return res
+};
+
+function getMaxLength(nums, index) {
+    if (memo[index] !== -1) {
+        return memo[index]
+    }
+
+    let res = 1
+    for (let i = 0; i < index; i++) {
+        if (nums[index] > nums[i]) {
+            res = Math.max(res, 1 + getMaxLength(nums, i))
+        }
+    }
+
+    return res
+}
---
+++ dp/longest-increasing-subsequence/lengthOfLIS-2.js
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/longest-increasing-subsequence/description/
+
+/**
+ * 思路：动态规划
+ * 时间复杂度: O(n ^ 2)
+ * 空间复杂度: O(n)
+ * @param {number[]} nums
+ * @return {number}
+ */
+const lengthOfLIS = function (nums) {
+    if (nums.length == 0) {
+        return 0
+    }
+
+    // memo[i] 表示以 nums[i] 为结尾的最长上升子序列的长度
+    const memo = new Array(nums.length).fill(1)
+    for (let i = 1; i < nums.length; i++) {
+        for (let j = 0; j < i; j++) {
+            if (nums[i] > nums[j]) {
+                memo[i] = Math.max(memo[i], 1 + memo[j])
+            }
+        }
+    }
+
+    let res = memo[0]
+    for (let i = 1; i < nums.length; i++) {
+        res = Math.max(res, memo[i])
+    }
+
+    return res
+}
