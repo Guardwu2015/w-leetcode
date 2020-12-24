1f84f2a02e555af0100ae8973a011493da324519
wuyue <wujingwei@mafengwo.cn>
2020-12-24 09:39:46 +0800

    feat: LC435. 无重叠区间(占坑，待完善)

---
+++ greedy/non-overlapping-intervals/eraseOverlapIntervals-1.js
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/non-overlapping-intervals/description/
+
+/**
+ * 思路：动态规划
+ * 时间复杂度: O(n^2)
+ * 空间复杂度: O(n)
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+const eraseOverlapIntervals = function (intervals) {
+}
---
+++ greedy/non-overlapping-intervals/eraseOverlapIntervals-2.js
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/non-overlapping-intervals/description/
+
+/**
+ * 思路：贪心算法
+ * 时间复杂度: O(n)
+ * 空间复杂度: O(n)
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+const eraseOverlapIntervals = function (intervals) {}
