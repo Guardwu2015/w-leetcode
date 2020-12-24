32eb49a1abfe7c9468cbaf7aaaab2bf7eb0e23e6
武岳 <guardwu@126.com>
2020-12-24 21:38:29 +0800

    feat: 完善LC435. 无重叠区间的解法

--- greedy/non-overlapping-intervals/eraseOverlapIntervals-1.js
+++ greedy/non-overlapping-intervals/eraseOverlapIntervals-1.js
@@ -7,5 +7,31 @@
  * @param {number[][]} intervals
  * @return {number}
  */
-const eraseOverlapIntervals = function (intervals) {
+const eraseOverlapIntervals = function(intervals) {
+        if(intervals.length == 0) {
+            return 0
+        }
+        
+        const newIntervals = intervals.sort((a, b) => {
+            const [sa, ea] = a
+            const [sb, eb] = b
+            if (sa !== sb) {
+                return sa - sb
+            }
+
+            return ea - eb
+        })
+        // memo[i]表示以intervals[i]为结尾的区间能构成的最长不重叠区间序列
+        const memo = new Array(intervals.length).fill(1)
+        for(let i = 1; i < newIntervals.length; i ++)
+            // memo[i]
+            for(let j = 0; j < i; j++)
+                if(intervals[i][0] >= intervals[j][1])
+                    memo[i] = Math.max(memo[i], 1 + memo[j])
+
+        let res = 0
+        for(let i = 0; i < memo.length; i ++)
+            res = Math.max(res, memo[i])
+
+        return intervals.length - res
 }
--- greedy/non-overlapping-intervals/eraseOverlapIntervals-2.js
+++ greedy/non-overlapping-intervals/eraseOverlapIntervals-2.js
@@ -7,4 +7,28 @@
  * @param {number[][]} intervals
  * @return {number}
  */
-const eraseOverlapIntervals = function (intervals) {}
+const eraseOverlapIntervals = function(intervals) {
+        if(intervals.length == 0) {
+            return 0
+        }
+
+        const newIntervals = intervals.sort((a, b) => {
+            const [sa, ea] = a
+            const [sb, eb] = b
+            if (ea !== eb) {
+                return ea - eb
+            }
+
+            return sa - sb
+        })
+
+        let res = 1
+        let pre = 0
+        for(let i = 1 ; i < newIntervals.length ; i ++)
+            if(newIntervals[i][0] >= newIntervals[pre][1]){
+                res ++
+                pre = i
+            }
+
+        return intervals.length - res
+};
